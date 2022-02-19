dc814cec00d90d71855d8bc7752c5f4d0fc37829
Thahir Thavvagunta <ahmedthahir369@gmail.com>
2022-02-19 12:40:54 +0530

    added configs in yml file

--- src/main/java/com/helloWorld/config/KafkaProducerConfig.java
+++ src/main/java/com/helloWorld/config/KafkaProducerConfig.java
@@ -3,6 +3,7 @@ package com.helloWorld.config;
 import com.helloWorld.dto.Student;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -16,11 +17,14 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfig {
 
+  @Value("${spring.kafka.producer.bootstrap-servers}")
+  private String bootstrapServers;
+
   @Bean
   public ProducerFactory<String, Student> studentProducerFactory() {
     Map<String, Object> configs = new HashMap<>();
 
-    configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
     configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
     configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
--- src/main/java/com/helloWorld/controller/KafkaProducerController.java
+++ src/main/java/com/helloWorld/controller/KafkaProducerController.java
@@ -2,6 +2,7 @@ package com.helloWorld.controller;
 
 import com.helloWorld.dto.Student;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,12 +13,13 @@ public class KafkaProducerController {
 
   @Autowired private KafkaTemplate<String, Student> kafkaTemplate;
 
-  private static final String TOPIC = "student-topic";
+  @Value("${spring.kafka.topic}")
+  private String studentTopic;
 
   @PostMapping("/publish")
   public String publishMessage(@RequestBody Student studentData) {
 
-    kafkaTemplate.send(TOPIC, studentData);
+    kafkaTemplate.send(studentTopic, studentData);
     return "Below data is published\n" + studentData;
   }
 }
